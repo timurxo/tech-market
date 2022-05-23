ba9dfc3ae56e29608c804bfaaba955f76a5e5724
timurxo <timurxo@gmail.com>
2022-05-22 19:39:14 -0500

    initial commit

---
+++ src/app/price-info/price-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-info',
+  templateUrl: './price-info.component.html',
+  styleUrls: ['./price-info.component.css']
+})
+export class PriceInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
